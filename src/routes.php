1bb602d2421810d28dfe787d280276dc603ac29a
DoogeJ <Jaap-Willem.Dooge@outlook.com>
2016-07-19 00:22:15 +0200

    Disabled logging for every visit (a default in the Slim 3.0 skeleton application)

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,8 @@
 // Routes
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    // disabled because logging every visit is silly - Nox
+    //$this->logger->info("Slim-Skeleton '/' route");
     
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
